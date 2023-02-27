446f0e1a65210edc562e60d6b92d1f232fc18e8a
Chu <1710518304@qq.com>
2023-02-27 23:06:47 +0800

    底部导航栏

--- src/components/tabl-bar/tab-bar.vue
+++ src/components/tabl-bar/tab-bar.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="tab-bar">
-    <div
-      class="tab-bar-item"
-      v-for="(item, index) in tabbarData"
-      :class="{ active: currentIndex === index }"
-      @click="itemClick(index, item)">
-      <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="" />
-      <img v-else :src="getAssetURL(item.activeImg)" alt="" />
-      <span class="text">{{ item.text }}</span>
-    </div>
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon>
+            <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="" />
+            <img v-else :src="getAssetURL(item.activeImg)" alt="" />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
@@ -16,40 +18,16 @@
   import tabbarData from '@/assets/localData/tabbar';
   import { getAssetURL } from '@/utils/load_assets';
   import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  const router = useRouter();
 
   const currentIndex = ref(0);
-  const itemClick = (index, item) => {
-    currentIndex.value = index;
-    router.push(item.path);
-  };
 </script>
 
 <style lang="less" scoped>
   .tab-bar {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    height: 50px;
-    border-top: 1px solid #f3f3f3;
-    .tab-bar-item {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      &.active {
-        color: var(--primary-color);
-      }
-      & > img {
-        width: 36px;
-      }
-      .text {
-        font-size: 12px;
-      }
+    // 局部修改
+    //--van-tabbar-item-icon-size: 28px;
+    img {
+      height: 26px;
     }
   }
 </style>
