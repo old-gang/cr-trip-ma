e3294eb88a6cb7e9455d1448274d777b4b887131
Chu <1710518304@qq.com>
2023-03-16 22:59:35 +0800

    详情页

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -4,6 +4,18 @@
     <div class="main" v-if="mainPart">
       <!-- 轮播图 -->
       <detailSwipe :swipe-data="mainPart.topModule.housePicture.housePics" />
+      <!-- 信息 -->
+      <detailInfo :top-infos="mainPart.topModule" />
+      <!-- 设施 -->
+      <detailFacility :house-facility="mainPart.dynamicModule.facilityModule.houseFacility" />
+      <!-- 房东 -->
+      <detailLandlord :landlord="mainPart.dynamicModule.landlordModule" />
+      <!-- 评论 -->
+      <detailComment :comment="mainPart.dynamicModule.commentModule" />
+      <!-- 须知 -->
+      <detailNotice :order-rules="mainPart.dynamicModule.rulesModule.orderRules" />
+      <!-- 周边 -->
+      <detailMap :position="mainPart.dynamicModule.positionModule" />
     </div>
   </div>
 </template>
@@ -12,7 +24,13 @@
   import { useRoute, useRouter } from 'vue-router';
   import { getDetailInfos } from '@/services';
   import { computed, ref } from 'vue';
-  import detailSwipe from './cpns/detail-swip.vue';
+  import detailSwipe from './cpns/detail_01-swip.vue';
+  import detailInfo from './cpns/detail_02-info.vue';
+  import detailFacility from './cpns/detail_03-facility.vue';
+  import detailLandlord from './cpns/detail_04-landlord.vue';
+  import detailComment from './cpns/detail_05-comment.vue';
+  import detailNotice from './cpns/detail_06-notice.vue';
+  import detailMap from './cpns/detail_07-map.vue';
   const router = useRouter();
   const route = useRoute();
 
